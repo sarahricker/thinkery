8d6b040ffa269fc7a5e99c79ae67d279290ae2d5
Madison Sadler <madison.sadler87@gmail.com>
2019-10-30 09:06:32 -0500

    updated CSS based on Sarah's PR suggestions

--- wp-content/themes/thinkery/functions.php
+++ wp-content/themes/thinkery/functions.php
@@ -147,8 +147,8 @@ remove_action( 'genesis_after_header', 'genesis_do_nav' );
 add_action( 'genesis_header', 'genesis_do_nav', 12 );
 
 // Repositions the secondary navigation menu.
-remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-add_action( 'genesis_footer', 'genesis_do_subnav', 10 );
+//remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+//add_action( 'genesis_footer', 'genesis_do_subnav', 10 );
 
 add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
 /**
