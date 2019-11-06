1ab8004804bdc229d15eecaf652a6668276bdf74
Sarah Ricker <sarah.ricker@wpengine.com>
2019-11-05 19:21:35 -0600

    Add intended thinkery color palette to gutenberg

--- wp-content/themes/thinkery/config/appearance.php
+++ wp-content/themes/thinkery/config/appearance.php
@@ -43,19 +43,64 @@ return [
 	'default-colors'       => $genesis_sample_default_colors,
 	'editor-color-palette' => [
 		[
-			'name'  => __( 'Custom color', 'genesis-sample' ), // Called “Link Color” in the Customizer options. Renamed because “Link Color” implies it can only be used for links.
-			'slug'  => 'theme-primary',
-			'color' => $genesis_sample_link_color,
-		],
-		[
-			'name'  => __( 'Primary color', 'genesis-sample' ),
+			'name'  => __( 'Primary color (Customizer)', 'genesis-sample' ),
 			'slug'  => 'theme-primary',
 			'color' => $genesis_sample_primary_color,
 		],
 		[
-			'name'  => __( 'Accent color', 'genesis-sample' ),
+			'name'  => __( 'Accent color (Customizer)', 'genesis-sample' ),
 			'slug'  => 'theme-secondary',
 			'color' => $genesis_sample_accent_color,
+		],
+		[
+			'name'  => __( 'Link color (Customizer)', 'genesis-sample' ),
+			'slug'  => 'theme-link',
+			'color' => $genesis_sample_link_color,
+		],
+		[
+			'name'  => __( 'Thinkery Red', 'genesis-sample' ),
+			'slug'  => 'thinkery-red',
+			'color' => '#db3e3d',
+		],
+		[
+			'name'  => __( 'Thinkery Orange', 'genesis-sample' ),
+			'slug'  => 'thinkery-orange',
+			'color' => '#e28413',
+		],
+		[
+			'name'  => __( 'Thinkery Navy Blue', 'genesis-sample' ),
+			'slug'  => 'thinkery-navy-blue',
+			'color' => '#151853',
+		],
+		[
+			'name'  => __( 'Thinkery Blue', 'genesis-sample' ),
+			'slug'  => 'thinkery-blue',
+			'color' => '#266dd3',
+		],
+		[
+			'name'  => __( 'Thinkery Green', 'genesis-sample' ),
+			'slug'  => 'thinkery-green',
+			'color' => '#00b9ae',
+		],
+		[
+			'name'  => __( 'Thinkery Dark Grey', 'genesis-sample' ),
+			'slug'  => 'thinkery-dark-grey',
+			'color' => '#1b2432',
+		],
+		[
+			'name'  => __( 'Thinkery Medium Grey', 'genesis-sample' ),
+			'slug'  => 'thinkery-medium-grey',
+			'color' => '#c4c5d4',
+		],
+		[
+			'name'  => __( 'Thinkery Light Grey', 'genesis-sample' ),
+			'slug'  => 'thinkery-light-grey',
+			'color' => '#e7ecf3',
+		],
+		[
+			'name'  => __( 'White', 'genesis-sample' ),
+			'slug'  => 'white',
+			'color' => '#ffffff',
 		],
 	],
 	'editor-font-sizes'    => [
